9de67a2fe398ab141dd2f9016d696dc19fe128b3
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2026-04-18 15:53:00 +0900

    feat: 헤더 수정

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,8 +4,8 @@ export default function Header() {
   return (
     <header className="absolute top-0 left-0 bg-black w-80 h-40 pl-10 gap-1 rounded-br-full flex items-center">
       {/* 햄버거 */}
-      <button>
-        <GiHamburgerMenu size={30} />
+      <button className="w-8 h-8 flex items-center justify-center transition-transform duration-200 hover:scale-[1.2]">
+        <GiHamburgerMenu className="w-full h-full" />
       </button>
       <span className="font-bold">어떤 영화를 볼까?</span>
     </header>
